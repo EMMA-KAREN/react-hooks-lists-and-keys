f1c2028885936d71fb74c72da7c158f3fd0b614b
EMMA KAREN <mumokaren26@gmail.com>
2024-11-15 03:30:57 +0300

    file

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -1,5 +1,9 @@
 import React from "react";
 
+function ColorItem(props) {
+  return <li style={{ color: props.color }}>{props.color}</li>;
+}
+
 function ColorList() {
   const colors = [
     "firebrick",
@@ -9,6 +13,13 @@ function ColorList() {
     "hotpink",
   ];
 
+  const colorElements = colors.map((color) => {
+    return (
+      <li key={color} style={{ color: color }}>
+        {color}
+      </li>
+    );
+  });
   return (
     <div>
       <h1>Top 5 CSS Colors</h1>
